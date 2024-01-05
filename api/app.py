cc0dbc46f7482d9128c43f9dc9d5e1befe743b77
Edward White <edwardwhite@dyn3154-2.wlan.ic.ac.uk>
2024-01-05 16:03:38 +0000

    first commit

---
+++ api/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     return "Hello World!!"
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    input_username = request.form.get("username")
+    input_password = request.form.get("password")
+    return render_template("success.html", username=input_username, password=input_password)
